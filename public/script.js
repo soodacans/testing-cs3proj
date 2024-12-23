a12c9677d984541412d3a21085f11612ce4fde3c
Glitch (glitch-hello-node) <none>
2024-12-23 09:04:16 +0000

    🌹🎾 Checkpoint ./public/dessertchoice.html:70042522/392 ./public/finish.html:70042522/602 ./public/script.js:70042522/524 ./public/homepage.html:70042522/193 ./public/icecream.html:70042522/52 ./public/cake.html:70042522/38 ./public/apronchoice.html:70042522/120

--- public/script.js
+++ public/script.js
@@ -1,16 +1,20 @@
+//When the user chooses to exit, this will close the window
 function exitGame() {
   window.close();
 }
 
+//When the user chooses to enter the game, this will bring them to the apron webpage
 function enterGame() {
   window.location.href = 'apronchoice.html'; 
 }
 
+//Once the user is done choosing, they will be brought to the dessert webpage to choose
 function selectApron(apron) {
 // Show dessert options
   window.location.href = 'dessertchoice.html';
 }
 
+//Depending on which the user chose, they will be brought to the corresponding webpage
 function chooseDessert(dessert) {
   if (dessert === 'cake') {
     window.location.href = 'cake.html';  
